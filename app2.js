ece8fafe2507a4f09b562e9f6955548777fcf93f
Kevin Wu <kevinnwu220@gmail.com>
2018-10-02 12:55:57 -0700

    convert to numbers

--- app2.js
+++ app2.js
@@ -1,4 +1,7 @@
-const Start = prompt('Please enter your start number')
-const End = prompt('Please enter your end number')
-const Step = prompt('Please enter your step number')
+const Start = prompt('Please enter your start number:')
+const End = prompt('Please enter your end number:')
+const Step = prompt('Please enter your step number:')
 
+const startNumber = Number(Start);
+const endNumber = Number(End);
+const stepNumber = Number(Step);
