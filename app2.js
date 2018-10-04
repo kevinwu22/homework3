3e384128362cb3b5d14afe8fdf014d3b24a8149b
Kevin Wu <kevinnwu220@gmail.com>
2018-10-03 20:39:38 -0700

    error msg revised

--- app2.js
+++ app2.js
@@ -1,35 +1,34 @@
 const Start = prompt('Please enter your start number:')
 const End = prompt('Please enter your end number:')
 const Step = prompt('Please enter your step number:')
-const Error = prompt('Error, you have entered an invalid number')
 
 const startNumber = Number(Start);
 const endNumber = Number(End);
 const stepNumber = Number(Step);
 
-if (Number.isInteger(startNumber) && startNumber != 0)
+if (Number.isInteger(startNumber) && startNumber !== 0)
 {
     startNumber = startNumber;
 }
 else
 {
-    document.getElementByID("error").innerHTML = Error;
+    document.getElementById("error").innerHTML = prompt('Error, you have entered an invalid start number');
 }
 
-if (Number.isInteger(endNumber) && endNumber != 0)
+if (Number.isInteger(endNumber) && endNumber !== 0)
 {
     endNumber = endNumber;
 }
 else 
 {
-    document.getElementByID("error").innerHTML = Error;
+    document.getElementById("error").innerHTML = prompt('Error you have entered an invalid end number');
 }
 
-if (Number.isInteger(stepNumber) && stepNumber != 0)
+if (Number.isInteger(stepNumber) && stepNumber !== 0)
 {
     stepNumber = stepNumber;
 }
 else
 {
-    document.getElementByID("error").innerHTML = Error;
+    document.getElementById("error").innerHTML = prompt('Error you have entered an invalid step number');
 }
