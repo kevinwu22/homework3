1906c744504261cfd447bbc24d439e9b22ed3184
Kevin Wu <kevinnwu220@gmail.com>
2018-10-03 20:55:24 -0700

    var

--- app2.js
+++ app2.js
@@ -2,9 +2,9 @@ const Start = prompt('Please enter your start number:')
 const End = prompt('Please enter your end number:')
 const Step = prompt('Please enter your step number:')
 
-const startNumber = Number(Start);
-const endNumber = Number(End);
-const stepNumber = Number(Step);
+var startNumber = Number(Start);
+var endNumber = Number(End);
+var stepNumber = Number(Step);
 
 if (Number.isInteger(startNumber) && startNumber !== 0)
 {
